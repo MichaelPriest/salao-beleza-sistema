ace7ca09ef1c47e8d9959896e8d430bf8e290c26
MichaelRaimundo <michael.rodrigoraimundo@gmail.com>
2026-03-13 23:23:50 -0300

    Update AuthClienteContext.js

--- src/contexts/AuthClienteContext.js
+++ src/contexts/AuthClienteContext.js
@@ -7,12 +7,12 @@ import { initializeApp } from 'firebase/app';
 
 // 🔥 Configuração do Firebase (use a mesma do seu projeto)
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyD7z7IjeHAa1BZayqyb4-ExmYz8xOYd5dA",
+  authDomain: "fluted-sentry-305001.firebaseapp.com",
+  projectId: "fluted-sentry-305001",
+  storageBucket: "fluted-sentry-305001.firebasestorage.app",
+  messagingSenderId: "386333037191",
+  appId: "1:386333037191:web:3b944b250bf676e1901e22"
 };
 
 // Inicializar Firebase
