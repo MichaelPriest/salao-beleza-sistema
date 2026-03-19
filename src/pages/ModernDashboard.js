67e1050e980a231d332568d840eef4dfb291b3b5
MichaelRaimundo <michael.rodrigoraimundo@gmail.com>
2026-03-18 23:09:33 -0300

    Update ModernDashboard.js

--- src/pages/ModernDashboard.js
+++ src/pages/ModernDashboard.js
@@ -1,5 +1,5 @@
 // src/pages/ModernDashboard.js - VERSÃO ATUALIZADA COM ANAMNESE
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import {
   Grid,
   Card,
